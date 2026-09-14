8b7cd858289c0dd58067944afa7828e3ccb74ee7
duyfelix1205-cell <duyfelix1205@gmail.com>
2026-09-15 03:08:20 +0700

    Update cobalt-fix.js

--- assets/js/cobalt-fix.js
+++ assets/js/cobalt-fix.js
@@ -4,11 +4,11 @@
   FISH.forEach(function (f) {
     const key = ((f.vn || '') + ' ' + (f.en || '')).toLowerCase();
     if (!/cá đĩa xanh cobalt|cobalt blue discus|xanh cobalt/.test(key)) return;
-    f.img = 'c_a_xanh_cobalt_1.jpg';
-    f.src = 'assets/img/c_a_xanh_cobalt_1.jpg';
+    f.img = 'xanh cobalt 1.webp';
+    f.src = 'assets/img/xanh cobalt 1.webp';
     f.gallery = [
-      'assets/img/c_a_xanh_cobalt_1.jpg',
-      'assets/img/c_a_xanh_cobalt_2.jpg'
+      'assets/img/xanh cobalt 1.webp',
+      'assets/img/xanh cobalt 2.jpg'
     ];
   });
 })();
